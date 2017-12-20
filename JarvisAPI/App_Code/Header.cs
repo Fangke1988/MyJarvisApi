1aa370e13275d3f9ec7fcccfd94b89109f0e7b0e
Fangke1988 <ghostfangke@163.com>
2017-12-20 12:44:35 +0800

    1
    test

---
+++ JarvisAPI/App_Code/Header.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Header 的摘要说明
+/// </summary>
+public class Header
+{
+    public Header()
+    {
+        //
+        // TODO: 在此处添加构造函数逻辑
+        //
+    }
+
+    private void AddLogs()
+    { }
+}
