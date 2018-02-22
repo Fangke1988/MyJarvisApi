8184b033f5b8ae2628eae02cd05c349642e18313
Fangke1988 <ghostfangke@163.com>
2018-02-22 16:28:29 +0800

    1

--- JarvisWCF/App_Code/TestConnectSVC.cs
+++ JarvisWCF/App_Code/TestConnectSVC.cs
@@ -16,7 +16,7 @@ using System.IO;
 // 注意: 使用“重构”菜单上的“重命名”命令，可以同时更改代码、svc 和配置文件中的类名“TestConnectSVC”。
 public class TestConnectSVC : ITestConnectSVC
 {
-    Tst ac = new Tst();
+    static Tst ac = new Tst();
     public void DoWork()
     {
      
